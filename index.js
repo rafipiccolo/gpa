510c795de7e4f8bac63fb145281411af0f9d8352
root <rafi.piccolo@gmail.com>
2021-01-20 21:50:39 +0100

    x

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function gpa(grade) {
     var order = ['F-', 'F', 'F+', 'E-', 'E', 'E+', 'D-', 'D', 'D+', 'C-', 'C', 'C+', 'B-', 'B', 'B+', 'A-', 'A', 'A+'];
 
-    grade = `${grade  }`;
+    grade = `${grade}`;
 
     if (grade.match(/^\d+$/)) {
         return order[grade] || '';
